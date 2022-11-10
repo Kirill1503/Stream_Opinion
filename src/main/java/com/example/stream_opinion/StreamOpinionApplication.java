8f7866d0754771802aa38b73573a404dd268549c
Kirill <K.katyshev1503@gmail.com>
2022-11-10 23:39:30 +0300

    Initial commit

---
+++ src/main/java/com/example/stream_opinion/StreamOpinionApplication.java
@@ -0,0 +1,13 @@
+package com.example.stream_opinion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamOpinionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StreamOpinionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stream_opinion/StreamOpinionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stream_opinion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamOpinionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
